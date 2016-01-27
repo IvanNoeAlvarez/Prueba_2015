da84416cdbbe860975765ee2b9328704f2ab45ea
Ivalion <Ivalion@Ivn.wifi.alumnado>
2016-01-27 20:30:33 +0100

    Proyectos

--- PacoEsUnCachondo/src/bombas/BOLADEDRAC.java
+++ PacoEsUnCachondo/src/bombas/BOLADEDRAC.java
@@ -4,19 +4,18 @@ public class BOLADEDRAC {
 
 	static String frase="";//palabra ordenada en mayus
 	static String abecedario ="ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
-	static String codificada="SDFRCHVCXQCFDFKRQGR";
+	static String codificada= "SDFRCHVCXQCFDFKRQGR";
 	static int desplazamiento=3;
 	static int posicion_abecedario;
 	public static void main(String[] args) {
+		System.out.println(codificada);
+		
 		for(int i=0;i<codificada.length();i++){
-			for(int n=0;i<abecedario.length();n++){
-				System.out.print(posicion_abecedario);
-				System.out.print(abecedario.charAt(n));
-				System.out.println("");
-				if (codificada.charAt(i)==abecedario.charAt(n))posicion_abecedario=n;//tengo el indice
-				if (posicion_abecedario<0) posicion_abecedario = (abecedario.length() + posicion_abecedario);
+			for(int n=0;n<abecedario.length();n++){
+				if (codificada.charAt(i)==abecedario.charAt(n)) posicion_abecedario=n-3;//tengo el indice
+				if (posicion_abecedario<0) posicion_abecedario = (posicion_abecedario + abecedario.length());
 			}
-			frase += abecedario.charAt(posicion_abecedario-3);
+			frase += abecedario.charAt(posicion_abecedario);
 		}
 		System.out.println(frase);
 	}
